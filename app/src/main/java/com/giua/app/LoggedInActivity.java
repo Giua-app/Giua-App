99ab1432b77283f059ffd54ffd372d6e3549b6ee
Franck1421 <51267237+Franck1421@users.noreply.github.com>
2021-05-27 17:29:30 +0200

    Changed show password button and other things

--- app/src/main/java/com/giua/app/LoggedInActivity.java
+++ app/src/main/java/com/giua/app/LoggedInActivity.java
@@ -63,25 +63,13 @@ public class LoggedInActivity extends AppCompatActivity {
         });
     }
 
+
+    /**
+     * Torna alla schermata di login quando si clicca il bottone per tornare indietro
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onBackPressed(){
+        Intent intent = new Intent(LoggedInActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/giua/app/MainActivity.java
+++ app/src/main/java/com/giua/app/MainActivity.java
@@ -1,40 +1,34 @@
 package com.giua.app;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.giua.webscraper.GiuaScraper;
+import com.giua.webscraper.GiuaScraperExceptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.concurrent.*;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.fragment.NavHostFragment;
 
-import android.os.Parcelable;
 import android.os.StrictMode;
 import android.text.InputType;
-import android.view.LayoutInflater;
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText user;
-    EditText password;
-    ProgressBar progressBar;
-    Button btnShowPassword;
+    EditText etUsername;
+    EditText etPassword;
+    ProgressBar pgProgressBar;
+    ImageButton btnShowPassword;
+    Button btnLogin;
     boolean btnShowActivated = false;
     GiuaScraper gS;
 
@@ -43,10 +37,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        user = findViewById(R.id.textUser);
-        password = findViewById(R.id.textPassword);
-        progressBar = findViewById(R.id.progressBar);
-        btnShowPassword = findViewById(R.id.btnShowPassword);
+        etUsername = findViewById(R.id.textUser);
+        etPassword = findViewById(R.id.textPassword);
+        pgProgressBar = findViewById(R.id.progressBar);
+        btnShowPassword = findViewById(R.id.show_password_button);
+        btnLogin = findViewById(R.id.login_button);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -66,19 +61,31 @@ public class MainActivity extends AppCompatActivity {
         /**
          * Login click listener
          */
-        findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progressBar.setVisibility(View.VISIBLE);
-                
-                gS = new GiuaScraper(user.getText().toString(), password.getText().toString());
+                pgProgressBar.setVisibility(View.VISIBLE);
+
+                try {
+                    gS = new GiuaScraper(etUsername.getText().toString(), etPassword.getText().toString());
+                } catch (GiuaScraperExceptions.SessionCookieEmpty sce){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Informazioni di login errate!", Toast.LENGTH_SHORT);
+                    toast.show();
+                    etPassword.setText("");
+                    pgProgressBar.setVisibility(View.INVISIBLE);
+                    return;
+                }
 
                 if(gS.checkLogin()){
                     System.out.println("login ok");
-                    //TODO: Fai partire la seconda activity
                     Intent intent = new Intent(MainActivity.this, LoggedInActivity.class);
                     intent.putExtra("giuascraper", gS);
                     startActivity(intent);
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Qualcosa e andato storto!", Toast.LENGTH_SHORT);
+                    toast.show();
+                    etPassword.setText("");
+                    pgProgressBar.setVisibility(View.INVISIBLE);
                 }
             }
         });
@@ -87,38 +94,29 @@ public class MainActivity extends AppCompatActivity {
         /**
          * Show password click listener
          */
-        findViewById(R.id.btnShowPassword).setOnClickListener(new View.OnClickListener() {
+        btnShowPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(!btnShowActivated) {
-                    password.setInputType (InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD | InputType.TYPE_CLASS_TEXT);      //Mostra la password
+                    etPassword.setInputType (InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD | InputType.TYPE_CLASS_TEXT);      //Mostra la password
+                    btnShowPassword.setImageResource(R.drawable.btn_show_password_true_image);
                 } else {
-                    password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);  //Nasconde la password
+                    etPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);  //Nasconde la password
+                    btnShowPassword.setImageResource(R.drawable.btn_show_password_false_image);
                 }
                 btnShowActivated = !btnShowActivated;
             }
         });
     }
 
+    /**
+     * Esci dall'applicazione simulando la pressione del tasto home
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onBackPressed(){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
