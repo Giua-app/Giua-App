ce8c02b24a7c155af0a9714eb1c530c61ed39ca5
HiemSword <hiemsword08@pm.me>
2021-07-18 13:12:31 +0200

    Added copyright notice on files and added copyright profile

--- app/src/main/java/com/giua/app/ui/lezioni/LessonView.java
+++ app/src/main/java/com/giua/app/ui/lezioni/LessonView.java
@@ -1,3 +1,22 @@
+/*
+ * Giua App
+ * Android app to view data from the giua@school workbook
+ * Copyright (C) 2021 - 2021 Hiem, Franck1421 and contributors
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
 package com.giua.app.ui.lezioni;
 
 import android.content.Context;
