e04440162e54c23a3d39b086bab6679def2947d6
HiemSword <hiemsword08@pm.me>
2022-01-06 23:22:36 +0100

    Fixed some bugs with unstable features

---
+++ app/src/main/java/com/giua/app/Secrets.kt
@@ -0,0 +1,15 @@
+package com.giua.app
+
+class Secrets {
+
+    //Method calls will be added by gradle task hideSecret
+    //Example : external fun getWellHiddenSecret(packageName: String): String
+
+    companion object {
+        init {
+            System.loadLibrary("secrets")
+        }
+    }
+
+    external fun getBaROGUEY(packageName: String): String
+}
