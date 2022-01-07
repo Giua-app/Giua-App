a4a995e1db3591b7d0ab42dd1d2be9a4b00ce3e9
HiemSword <hiemsword08@pm.me>
2022-01-07 17:32:24 +0100

    Modified BugReportActivity (ui is broken help)

--- app/src/main/java/com/giua/app/BugReportActivity.java
+++ app/src/main/java/com/giua/app/BugReportActivity.java
@@ -20,10 +20,14 @@
 package com.giua.app;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.giua.utils.JsonBuilder;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
 import org.jsoup.Connection;
@@ -34,7 +38,10 @@ public class BugReportActivity extends AppCompatActivity {
     Button btnSend;
     TextInputLayout bugTitle;
     TextInputLayout bugDesc;
+    ProgressBar progressBar;
     String dontStealMePlease = "dontStealMePlease";
+    String stacktrace;
+    boolean fromCAOC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,24 +51,53 @@ public class BugReportActivity extends AppCompatActivity {
         btnSend = findViewById(R.id.bug_send_button);
         bugTitle = findViewById(R.id.txtBugTitle);
         bugDesc = findViewById(R.id.txtBugDesc);
+        progressBar = findViewById(R.id.bug_progressbar);
 
         btnSend.setOnClickListener(v -> onSendBug());
         dontStealMePlease = BuildConfig.SECRET_KEY;
+
+        fromCAOC = getIntent().getBooleanExtra("fromCAOC", false);
+
+        if(fromCAOC){
+            stacktrace = getIntent().getStringExtra("stacktrace");
+        }
     }
 
-    private void onSendBug(){
+    private void onSendBug() {
+        btnSend.setVisibility(View.INVISIBLE);
+        bugTitle.setEnabled(false);
+        bugDesc.setEnabled(false);
+        progressBar.setVisibility(View.VISIBLE);
+
         new Thread(() -> {
+            String body = "**Descrizione del bug**\n" +
+                    bugDesc.getEditText().getText() + "\n\n" +
+                    "**Indica i passaggi per riprodurre il bug**\n" +
+                    "1. Andare su...\n" +
+                    "2. Premere...\n" +
+                    "3. Cliccare su...\n\n";
+
+            if(fromCAOC){
+                body += "\n\n**Stacktrace**\n" + stacktrace;
+            }
+
             try {
                 Connection session = Jsoup.newSession().ignoreContentType(true);
                 session.url("https://api.github.com/repos/HiemSword/regnodellasintassi-site/issues")
                         .header("Authorization", "token " + new Secrets().getgEPeTNbQ(getPackageName()) + dontStealMePlease)
                         .header("Accept", "application/vnd.github.v3+json")
                         .requestBody("{\"title\": \"" + bugTitle.getEditText().getText() + "\"," +
-                                "\"body\": \"" + bugDesc.getEditText().getText() + "\"}")
+                                "\"body\": \"" + JsonBuilder.escape(body) + "\"}")
                         .post();
             } catch(Exception e){
-                e.printStackTrace();
+                Snackbar.make(findViewById(android.R.id.content), "Errore durante l'invio del Bug Report", Snackbar.LENGTH_SHORT).show();
             }
+            runOnUiThread(() -> {
+                btnSend.setVisibility(View.VISIBLE);
+                bugTitle.setEnabled(true);
+                bugDesc.setEnabled(true);
+                progressBar.setVisibility(View.INVISIBLE);
+            });
         }).start();
     }
 }
